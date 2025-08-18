4f9dbd2dc97a237d72eb6711efc568f715da1475
Saurav Mishra <80103738+SauravBizbRolly@users.noreply.github.com>
2025-08-18 18:47:28 +0530

    Update SmsSchedulerService.java

--- src/main/java/com/iemr/flw/service/impl/SmsSchedulerService.java
+++ src/main/java/com/iemr/flw/service/impl/SmsSchedulerService.java
@@ -47,11 +47,14 @@ public class SmsSchedulerService {
     @Autowired
     private CookieUtil cookieUtil;
 
-    @Scheduled(cron = "0 0 9 * * *")
+    // @Scheduled(cron = "0 0 9 * * *")
     public void sendAncReminders() {
         try {
 
+            LocalDate tomorrow2 = LocalDate.now().plusDays(1);
 
+            smsServiceImpl.sendReminderSMS("9560618681","ANC1",tomorrow2);
+            
             List<ANCVisit> ancVisitList = ancVisitRepo.findAll();
             logger.info("ANC_SMS service is start");
             ancVisitList.forEach(ancVisit -> {
@@ -90,7 +93,12 @@ public class SmsSchedulerService {
             logger.error(e.getMessage());
         }
     }
-    
+
+    @PostConstruct
+    public void triggerAncRemindersOnce() {
+        sendAncReminders();
+    }
+
 
     @Scheduled(cron = "0 0 9 * * *")
     public void trigerTomorrowImmunizationReminders() {
