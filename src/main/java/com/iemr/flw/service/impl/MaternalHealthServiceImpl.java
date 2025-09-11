be22e16645f60fc7cc40a9eb9bcb35babbc97794
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-09-11 17:00:02 +0530

    fix get ben data issue

--- src/main/java/com/iemr/flw/service/impl/MaternalHealthServiceImpl.java
+++ src/main/java/com/iemr/flw/service/impl/MaternalHealthServiceImpl.java
@@ -14,11 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
