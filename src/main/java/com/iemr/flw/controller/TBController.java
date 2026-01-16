2aa90c8238c48442e873330b6811917e79106ff2
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-01-16 16:49:13 +0530

    Tb confirmed cases

--- src/main/java/com/iemr/flw/controller/TBController.java
+++ src/main/java/com/iemr/flw/controller/TBController.java
@@ -1,14 +1,17 @@
 package com.iemr.flw.controller;
 
+import com.iemr.flw.domain.iemr.TBConfirmedCaseDTO;
 import com.iemr.flw.dto.identity.GetBenRequestHandler;
 import com.iemr.flw.dto.iemr.TBScreeningRequestDTO;
 import com.iemr.flw.dto.iemr.TBSuspectedRequestDTO;
+import com.iemr.flw.service.TBConfirmedCaseService;
 import com.iemr.flw.service.TBScreeningService;
 import com.iemr.flw.service.TBSuspectedService;
 import com.iemr.flw.utils.response.OutputResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 
+import org.checkerframework.checker.units.qual.A;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +30,9 @@ public class TBController {
 
     @Autowired
     private TBSuspectedService tbSuspectedService;
+
+    @Autowired
+    private TBConfirmedCaseService tbConfirmedCaseService;
 
     @Operation(summary = "get tb screening data of all beneficiaries registered with given user id")
     @RequestMapping(value = { "/screening/getAll" }, method = { RequestMethod.POST })
@@ -124,5 +130,65 @@ public class TBController {
         }
         return response.toString();
     }
+
+    @Operation(summary = "save tb confirmed case data of beneficiary")
+    @RequestMapping(value = { "/confirmed/save" }, method = { RequestMethod.POST })
+    public String saveConfirmedCase(
+            @RequestBody TBConfirmedCaseDTO requestDTO,
+            @RequestHeader(value = "jwtToken") String token) {
+
+        OutputResponse response = new OutputResponse();
+
+        try {
+            if (requestDTO != null) {
+
+                logger.info("request object with timestamp : "
+                        + new Timestamp(System.currentTimeMillis()) + " "
+                        + requestDTO);
+
+                String result = tbConfirmedCaseService.save(requestDTO, token);
+
+                if (result != null)
+                    response.setResponse(result);
+                else
+                    response.setError(500, "No record saved");
+
+            } else {
+                response.setError(500, "Invalid/NULL request obj");
+            }
+        } catch (Exception e) {
+            logger.error("Error in save tb confirmed case details : ", e);
+            response.setError(500, "Error in save tb confirmed case details : " + e.getMessage());
+        }
+
+        return response.toString();
+    }
+
+    @Operation(summary = "get tb confirmed case by beneficiary id")
+    @RequestMapping(value = { "/confirmed/getAll}" }, method = { RequestMethod.GET })
+    public String getConfirmedByBenId(
+            @PathVariable Long benId,
+            @RequestHeader(value = "jwtToken") String token) {
+
+        OutputResponse response = new OutputResponse();
+
+        try {
+            String result =
+                    tbConfirmedCaseService.getByUserId(token);
+
+            if (result != null)
+                response.setResponse(result);
+            else
+                response.setError(404, "No record found");
+
+        } catch (Exception e) {
+            logger.error("Error in get tb confirmed case details : ", e);
+            response.setError(500, "Error in get tb confirmed case details : " + e.getMessage());
+        }
+
+        return response.toString();
+    }
+
+
 
 }
