539a3e16851b69c08ac105d1ada3fd92af0a9787
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-01-16 17:19:10 +0530

    Tb confirmed cases

--- src/main/java/com/iemr/flw/repo/iemr/TBConfirmedTreatmentRepository.java
+++ src/main/java/com/iemr/flw/repo/iemr/TBConfirmedTreatmentRepository.java
@@ -1,6 +1,6 @@
 package com.iemr.flw.repo.iemr;
 
-import com.iemr.flw.dto.iemr.TBConfirmedCase;
+import com.iemr.flw.domain.iemr.TBConfirmedCase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/iemr/flw/service/TBConfirmedCaseService.java
+++ src/main/java/com/iemr/flw/service/TBConfirmedCaseService.java
@@ -1,11 +1,7 @@
 package com.iemr.flw.service;
 
 import com.iemr.flw.domain.iemr.TBConfirmedCaseDTO;
-import com.iemr.flw.dto.iemr.TBConfirmedCase;
-import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public interface TBConfirmedCaseService {
