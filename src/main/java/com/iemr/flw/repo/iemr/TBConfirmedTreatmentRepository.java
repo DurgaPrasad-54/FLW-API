129fa37640e10d2525f82f73ef9d00ff1c6aa590
Saurav Mishra <80103738+SauravBizbRolly@users.noreply.github.com>
2026-01-19 19:46:39 +0530

    Update TBConfirmedTreatmentRepository.java

--- src/main/java/com/iemr/flw/repo/iemr/TBConfirmedTreatmentRepository.java
+++ src/main/java/com/iemr/flw/repo/iemr/TBConfirmedTreatmentRepository.java
@@ -14,5 +14,4 @@ public interface TBConfirmedTreatmentRepository
     List<TBConfirmedCase> findByBenId(Long benId);
     List<TBConfirmedCase> findByUserId(Integer benId);
 
-    Optional<TBConfirmedCase> findByBenIdAndSuspectedTbId(Long benId, Integer suspectedTbId);
 }
